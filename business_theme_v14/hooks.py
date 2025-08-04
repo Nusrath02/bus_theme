df6868a1b89fa9efaecf0f7e180385b8a6d1a9e8
Nusrath02 <nusrath.s@itchamps.com>
2025-08-04 12:26:34 +0530

    Update hooks.py

--- business_theme_v14/hooks.py
+++ business_theme_v14/hooks.py
@@ -1,17 +1,14 @@
 from . import __version__ as app_version
 
 app_name = "business_theme_v14"
-app_title = "Business Theme V14 - Sky Blue Edition"
+app_title = "Business Theme V14"
 app_publisher = "Midocean Technologies Pvt Ltd"
-app_description = "Business Theme for ERPNext / Frappe with Sky Blue Design"
+app_description = "Business Theme for ERPNext / Frappe"
 app_icon = "octicon octicon-file-directory"
-app_color = "blue"
+app_color = "grey"
 app_email = "sagar@midocean.tech"
 app_license = "MIT"
 
-# Includes in <head>
-# ------------------
-
-# include js, css files in header of desk.html
+# Simple includes - just CSS and JS
 app_include_css = "/assets/business_theme_v14/css/business_theme_v14.css"
 app_include_js = "/assets/business_theme_v14/js/business_theme_v14.js"
