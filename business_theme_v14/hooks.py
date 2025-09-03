bb9311d4a844fa4d90069ef529b72b23ab79f421
AbhishekN0174 <abhishek.n@itchamps.com>
2025-09-03 10:56:57 +0530

    Update hooks.py

--- business_theme_v14/hooks.py
+++ business_theme_v14/hooks.py
@@ -13,7 +13,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/business_theme_v14/css/business_theme_v14.css"
+app_include_css = "/assets/business_theme_v14/css/custom_theme.css"
 
 
 
