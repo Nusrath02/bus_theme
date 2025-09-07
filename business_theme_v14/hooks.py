b75b519cfd67bdf17f913ba5de7a33b2e51284d8
AbhishekN0174 <abhishek.n@itchamps.com>
2025-09-07 17:23:53 +0530

    Update hooks.py

--- business_theme_v14/hooks.py
+++ business_theme_v14/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 app_include_css = "/assets/business_theme_v14/css/custom_theme.css"
 # app_include_css = "/assets/business_theme_v14/css/business_theme_v14.css"
 
-# custom_theme
+
 
 # app_include_js = "/assets/business_theme_v14/js/business_theme_v14.js"
 app_include_js = "/assets/business_theme_v14/js/Horizontal.js"
