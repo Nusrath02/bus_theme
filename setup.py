0804804dfbe3a4110ef73321169f1c8166280b9f
Nusrath02 <nusrath.s@itchamps.com>
2025-10-06 13:11:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,3 +17,46 @@ setup(
 	include_package_data=True,
 	install_requires=install_requires
 )
+
+import frappe
+
+NAVBAR_ITEM_LABEL = "Show Time"
+NAVBAR_EXTND_ITEMS = [
+    {
+        "item_label": NAVBAR_ITEM_LABEL,
+        "item_type": "Action",
+        "action": "() => {}",
+        "idx": 1,
+    },
+    {
+        "item_type": "Separator",
+        "idx": 2,
+    },
+]
+
+def after_install():
+    if frappe.db.exists("Navbar Item", {"item_label": NAVBAR_ITEM_LABEL}):
+        return
+    navbar_settings = frappe.get_single("Navbar Settings")
+    for ni in navbar_settings.settings_dropdown:
+        ni.idx = ni.idx + len(NAVBAR_EXTND_ITEMS)
+    navbar_settings.extend("settings_dropdown", NAVBAR_EXTND_ITEMS)
+    navbar_settings.save()
+    frappe.db.commit()
+
+def after_uninstall():
+    if not frappe.db.exists("Navbar Item", {"item_label": NAVBAR_ITEM_LABEL}):
+        return
+    patch_flag = frappe.flags.in_patch
+    frappe.flags.in_patch = True
+    navbar_settings = frappe.get_single("Navbar Settings")
+    for i, ni in enumerate(navbar_settings.settings_dropdown):
+        if ni.item_label == NAVBAR_ITEM_LABEL:
+            navbar_settings.settings_dropdown.pop(i)
+            # Fix: item_stype should be item_type
+            if i < len(navbar_settings.settings_dropdown) and navbar_settings.settings_dropdown[i].item_type == "Separator":
+                navbar_settings.settings_dropdown.pop(i)
+            break
+    navbar_settings.save()
+    frappe.db.commit()
+    frappe.flags.in_patch = patch_flag
