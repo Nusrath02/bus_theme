f6922dff284d8f8632b39e14559bfccb626fb6f7
Nusrath02 <nusrath.s@itchamps.com>
2025-10-09 09:56:06 +0530

    Update hooks.py

--- business_theme_v14/hooks.py
+++ business_theme_v14/hooks.py
@@ -19,12 +19,12 @@ app_version = str(int(time.time()))  # current timestamp, changes every deploy
 # include js, css files in header of desk.html
 app_include_css = [
     f"/assets/business_theme_v14/css/custom_theme.css?v={app_version}"
-    f"/assets/business_theme_v14/css/navbar_extended.css?v={app_version}"
+   
 ]
 
 app_include_js = [
     "/assets/business_theme_v14/js/sycone_footer.js",  # Added missing comma
-    "/assets/business_theme_v14/js/desk_navbar_extended.js"
+
 ]
 
 # Favicon
